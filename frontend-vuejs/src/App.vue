81daf6f3181089eb51a1e36c94c279113d7d7047
perotedev <rodripema@hotmail.com>
2022-03-24 16:53:45 -0400

    [FEAT]view01 - Adicionado Componente SliderTop e imagens assets

--- frontend-vuejs/src/App.vue
+++ frontend-vuejs/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="m-0 p-0">
+    <SliderTop/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SliderTop from './components/SliderTop.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SliderTop
   }
 }
 </script>
@@ -21,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ frontend-vuejs/src/components/SliderTop.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="../assets/banners/grupo-mascara-7.png" class="d-block w-100" alt="Bem Vindo à Sua Nova Hamburgueria">
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/banners/grupo-mascara-9.png" class="d-block w-100" alt="Drink Michigan Beer">
+      </div>
+      <div class="carousel-item">
+        <img src="../assets/banners/grupo-mascara-10.png" class="d-block w-100" alt="Conheça a Trajetória da Duc Lanches">
+      </div>
+    </div>
+  </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SliderTop",
+  created() {},
+  mounted() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
