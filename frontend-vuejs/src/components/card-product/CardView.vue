614a782f8a73d4a7a65369078128ed40f9b7bcb4
perotedev <rodripema@hotmail.com>
2022-03-26 19:21:39 -0400

    [FEAT]view01 - Responsividade da descriçao do burger e dos botoes plus e minus

--- frontend-vuejs/src/components/card-product/CardView.vue
+++ frontend-vuejs/src/components/card-product/CardView.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="mb-3 rad-10 bg-card-product product-card d-flex">
-    <div class="row ps-2 align-items-center">
-      <div class="col-3">
+  <div class="mb-3 rad-12 bg-card-product product-card d-flex justify-content-between">
+    <div class="row ps-2 align-items-center product-description">
+      <div class="col-4">
         <img class="img-card" :src="imgProduct" :alt="titleProduct"/>
       </div>
 
-      <div class="col-9 text-start">
+      <div class="col-8 text-start">
         <p class="p-0 m-0 f-title">{{ titleProduct }}</p>
-        <p class="p-0 m-0 mt-1">{{ descriptionProduct }}</p>
+        <p class="p-0 m-0 mt-1 text-description">{{ descriptionProduct }}</p>
         <PriceProduct  :price="price" :promotion="promotion"/>
       </div>
     </div>
-    <div class="align-self-end p-0 m-0">
-      <div class="btn-yellow btn-select">
-        <i class="fi bi-plus"></i>
+    <div class="align-self-end">
+      <div v-if="showBtnMinus" class="d-flex">
+        <div class="btn-yellow btn-select-minus">
+          <p class="p-0 m-0 fs-2 btn-minus">-</p>
+        </div>
+        <div class="btn-yellow btn-select-plus2">
+          <p class="p-0 m-0 fs-4 btn-plus">+</p>
+        </div>
+      </div>
+      <div v-else class="btn-yellow btn-select-plus">
+          <p class="p-0 m-0 fs-4 btn-plus">+</p>
       </div>
     </div>
   </div>
@@ -39,7 +47,8 @@ export default {
       titleProduct: "Hamburguer Artesanal",
       descriptionProduct: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
       price: 12.57,
-      promotion: false
+      promotion: false,
+      showBtnMinus: false
     };
   },
   props: {},
@@ -56,16 +65,56 @@ export default {
   height: 125px;
 }
 
-.img-card {
-  width: 100px;
-  height: auto;
-} 
+.product-description {
+  max-width: 92%;
+}
 
-.btn-select {
-  width: 30px;
+.text-description {
+  text-overflow: ellipsis;
+  display: -webkit-box !important;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  white-space: normal;
+  overflow: hidden;
+}
+
+.img-card {
+  max-height: 100px;
+  max-width: 95%;
+}
+
+.btn-minus {
+  color: #232227;
+  position: relative;
+  top: -15px;
+}
+
+.btn-plus {
+  color: #232227;
+  position: relative;
+  top: -9px;
+}
+
+.btn-select-minus {
+  width: 25px;
   height: 25px;
-  border-bottom-right-radius: 8px;
-  border-top-left-radius: 8px;
+  border-top-left-radius: 12px;
+  border-right: solid 1px #F9A332;
+  cursor: pointer;
+}
+
+.btn-select-plus {
+  width: 25px;
+  height: 25px;
+  border-bottom-right-radius: 12px;
+  border-top-left-radius: 12px;
+  cursor: pointer;
+}
+
+.btn-select-plus2 {
+  width: 25px;
+  height: 25px;
+  border-bottom-right-radius: 12px;
   cursor: pointer;
 }
 </style>
