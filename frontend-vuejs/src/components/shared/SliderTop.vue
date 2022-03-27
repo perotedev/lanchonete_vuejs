7f1881b634ecec80afd84ec27db81d79a9a59854
perotedev <rodripema@hotmail.com>
2022-03-27 00:36:17 -0400

    [FEAT]view01 - Botoes guias do carousel do bootstrap personalizados

--- frontend-vuejs/src/components/shared/SliderTop.vue
+++ frontend-vuejs/src/components/shared/SliderTop.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img :src="source1" class="d-block w-100" alt="Bem Vindo à Sua Nova Hamburgueria">
@@ -12,7 +12,13 @@
           <img :src="source3" class="d-block w-100" alt="Conheça a Trajetória da Duc Lanches">
         </div>
       </div>
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active rounded-circle" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" class="rounded-circle" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" class="rounded-circle" aria-label="Slide 3"></button>
+      </div>
     </div>
+
   </section>
 </template>
 
@@ -32,3 +38,25 @@ export default {
   methods: {},
 };
 </script>
+
+<style scoped>
+.carousel-indicators {
+  margin-top: 10px;
+  position: relative;
+}
+ 
+.carousel-indicators button {
+  width: 10px;
+  height: 10px;
+  background-color: #232227;
+  border: solid 1px #FFBC00;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.carousel-indicators button.active {
+  background-color: #FFBC00;
+  transition-duration: 200ms;
+}
+
+</style>
