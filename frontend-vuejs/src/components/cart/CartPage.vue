798f1b039bf78a6b7e9a87805ec4161225c425b5
perotedev <rodripema@hotmail.com>
2022-03-27 19:43:13 -0400

    [FEAT]view01 - Mensagem de carrinho vazio

--- frontend-vuejs/src/components/cart/CartPage.vue
+++ frontend-vuejs/src/components/cart/CartPage.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <div class="row">
+    <div v-if="haveProduct" class="row cart-products">
       <div class="col-12 col-md-6 col-xxl-4" v-for="(product, index) in list" :key="index">
         <CardCartProduct class="fade-in" />
       </div>
     </div>
-
+    <div v-else class="cart-products pt-5">
+      <div class="fs-2">
+        <p><i class="bi bi-emoji-neutral me-2"></i> Ops! No momento não há itens no carrinho</p>
+      </div>
+    </div>
+    
     <div class="row">
-      <div class="col-12 col-lg-6">
-        <btn>
-          
+      <div class="col-12 col-lg-6 mt-3 pt-3 text-start ps-5">
+        <btn v-show="haveProduct" class="btn-exclude-all rad-10 f-title px-3 py-2">
+          Limpar Carrinho
         </btn>
       </div>
 
@@ -32,10 +37,14 @@ export default {
     CardCartProduct
   },
   created() {},
+  mounted() {
+    this.haveProduct = this.list.length > 0;
+  },
   data() {
     return {
-      list: ["1", "2", "3", "4"],
-      totalCompras: 22.45
+      list: [1,2,3,4],
+      totalCompras: 22.45,
+      haveProduct: Boolean
     };
   },
   props: {},
@@ -48,4 +57,21 @@ export default {
   background-color: white;
   color: #232227;
 }
+
+.cart-products {
+  min-height: 312px;
+}
+
+.btn-exclude-all {
+  border: none;
+  outline: none;
+  color: white;
+  cursor: pointer;
+  background-color: #F12446;
+}
+
+.btn-exclude-all:hover {
+  background-color: #F5435C;
+  transition-duration: 200ms;
+} 
 </style>
